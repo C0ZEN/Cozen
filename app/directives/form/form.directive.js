61d6b13d9ada2e879c36080d055d8d3435be98f5
Cozen <geoffrey.testelin@gmail.com>
2017-01-10 09:35:48 +0100

    form - has error (experimental)

--- app/directives/form/form.directive.js
+++ app/directives/form/form.directive.js
@@ -7,90 +7,94 @@
  * @description
  *
  * [Attributes params]
- * @param {number} cozenFormId   > Id of the form
- * @param {string} cozenFormName > Name of the form [required]
- * @param {string} cozenFormCtrl > Parent controller
+ * @param {number} cozenFormId    > Id of the form
+ * @param {string} cozenFormName  > Name of the form [required]
+ * @param {string} cozenFormModel > Model [required]
  *
  */
 (function (angular) {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('cozenLib.form', [])
-        .directive('cozenForm', cozenForm);
+  angular
+    .module('cozenLib.form', [])
+    .directive('cozenForm', cozenForm);
 
-    cozenForm.$inject = [
-        '$timeout'
-    ];
+  cozenForm.$inject = [
+    '$timeout'
+  ];
 
-    function cozenForm($timeout) {
-        return {
-            link       : link,
-            restrict   : 'E',
-            replace    : false,
-            transclude : true,
-            templateUrl: 'directives/form/form.template.html'
+  function cozenForm($timeout) {
+    return {
+      link       : link,
+      restrict   : 'E',
+      replace    : false,
+      transclude : true,
+      templateUrl: 'directives/form/form.template.html'
+    };
+
+    function link(scope, element, attrs) {
+      var methods = {
+        init        : init,
+        hasError    : hasError,
+        destroy     : destroy,
+        dispatchName: dispatchName
+      };
+
+      var data = {
+        directive: 'cozenForm'
+      };
+
+      scope._isReady = false;
+
+      methods.init();
+
+      function init() {
+
+        // Public functions
+        scope._methods = {
+          dispatchName: dispatchName
         };
 
-        function link(scope, element, attrs) {
-            var methods = {
-                init        : init,
-                hasError    : hasError,
-                destroy     : destroy,
-                dispatchName: dispatchName
-            };
+        // Checking required stuff
+        if (methods.hasError()) return;
 
-            var data = {
-                directive: 'cozenForm'
-            };
+        // Default values (attributes)
+        scope._cozenFormId    = angular.isDefined(attrs.cozenFormId) ? attrs.cozenFormId : '';
+        scope._cozenFormName  = attrs.cozenFormName;
+        scope._cozenFormModel = attrs.cozenFormModel;
 
-            scope._isReady = false;
+        // Init stuff
+        element.on('$destroy', methods.destroy);
 
-            methods.init();
+        // Display the template
+        scope._isReady = true;
+      }
 
-            function init() {
-
-                // Public functions
-                scope._methods = {
-                    dispatchName: dispatchName
-                };
-
-                // Checking required stuff
-                if (methods.hasError()) return;
-
-                // Default values (attributes)
-                scope._cozenFormId   = angular.isDefined(attrs.cozenFormId) ? attrs.cozenFormId : '';
-                scope._cozenFormName = attrs.cozenFormName;
-                scope._cozenFormCtrl = angular.isDefined(attrs.cozenFormCtrl) ? attrs.cozenFormCtrl : 'vm';
-
-                // Init stuff
-                element.on('$destroy', methods.destroy);
-
-                // Display the template
-                scope._isReady = true;
-            }
-
-            function hasError() {
-                if (Methods.isNullOrEmpty(attrs.cozenFormName)) {
-                    Methods.directiveErrorRequired(data.directive, 'Name');
-                    return true;
-                }
-                return false;
-            }
-
-            function destroy() {
-                element.off('$destroy', methods.destroy);
-            }
-
-            function dispatchName() {
-                $timeout(function () {
-                    scope.$broadcast('cozenFormName', {
-                        name: scope._cozenFormCtrl + '.' + scope._cozenFormName
-                    });
-                }, 1);
-            }
+      function hasError() {
+        if (Methods.isNullOrEmpty(attrs.cozenFormName)) {
+          Methods.directiveErrorRequired(data.directive, 'Name');
+          return true;
         }
+        if (Methods.isNullOrEmpty(attrs.cozenFormModel)) {
+          Methods.directiveErrorRequired(data.directive, 'Model');
+          return true;
+        }
+        return false;
+      }
+
+      function destroy() {
+        element.off('$destroy', methods.destroy);
+      }
+
+      function dispatchName() {
+        $timeout(function () {
+          scope.$broadcast('cozenFormName', {
+            name: scope._cozenFormName
+          });
+        }, 1);
+      }
     }
+  }
 
 })(window.angular);
 
