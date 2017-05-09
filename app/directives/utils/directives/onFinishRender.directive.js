4135acfe3702c360cec507726c25bb766d76312e
Cozen <geoffrey.testelin@gmail.com>
2017-05-09 19:21:07 +0200

    Added new directive onFinishRender to send event when a ng-repeat has finished his work

---
+++ app/directives/utils/directives/onFinishRender.directive.js
@@ -0,0 +1,44 @@
+/**
+ * @ngdoc directive
+ * @name cozen-on-finish-render
+ * @scope
+ * @restrict A
+ * @replace false
+ * @transclude false
+ * @description
+ *
+ * @param {string} cozenOnFinishRender = cozenFinishedRender > Name of the emitted event
+ *
+ */
+(function (angular) {
+    'use strict';
+
+    angular
+        .module('cozenLib')
+        .directive('cozenOnFinishRender', cozenOnFinishRender);
+
+    cozenOnFinishRender.$inject = [
+        '$timeout'
+    ];
+
+    function cozenOnFinishRender($timeout) {
+        return {
+            link      : link,
+            restrict  : 'A',
+            replace   : false,
+            transclude: false
+        };
+
+        function link(scope, element, attrs) {
+
+            // Default values (attributes)
+            scope._cozenOnFinishRender = Methods.isNullOrEmpty(attrs.cozenOnFinishRender) ? 'cozenFinishedRender' : attrs.cozenOnFinishRender;
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit(scope._cozenOnFinishRender);
+                });
+            }
+        }
+    }
+
+})(window.angular);
