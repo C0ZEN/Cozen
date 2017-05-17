8ae82f933a3614605f44ae8df0414627bb59cdfa
Testelin Geoffrey <geoffrey.testelin@gmail.com>
2017-05-17 10:53:43 +0200

    New attr cozenBtnUploadErrorDesign to toggle error style on upload. New attr cozenBtnUploadSuccessDesign to toggle success style on upload. Tee upload btn have now a style when the file is on error or on success.

---
+++ release/clean/main.js
@@ -0,0 +1,58 @@
+(function (angular) {
+    'use strict';
+
+    if (typeof module !== 'undefined' &&
+        typeof exports !== 'undefined' &&
+        module.exports === exports) {
+        module.exports = 'cozen-lib';
+    }
+
+    /**
+     * @ngdoc overview
+     * @name cozenLib
+     * @description
+     * # cozenLib
+     *
+     * Main module of the application.
+     */
+    angular
+        .module('cozenLib', [
+            'ngAnimate',
+            'ngAria',
+            'ngCookies',
+            'ngMessages',
+            'ngResource',
+            'ngRoute',
+            'ngSanitize',
+            'ngTouch',
+            'ui.router',
+            'pascalprecht.translate',
+            'ui.bootstrap',
+            'ui.bootstrap.tooltip',
+            'ngScrollbars',
+            'uuid',
+            'monospaced.elastic',
+            'ngFileUpload',
+
+            'cozenLib.alert',
+            'cozenLib.btn',
+            'cozenLib.btnCheck',
+            'cozenLib.btnLazyTest',
+            'cozenLib.btnRadio',
+            'cozenLib.btnToggle',
+            'cozenLib.dropdown',
+            'cozenLib.form',
+            'cozenLib.input',
+            'cozenLib.list',
+            'cozenLib.pagination',
+            'cozenLib.panel',
+            'cozenLib.pills',
+            'cozenLib.popup',
+            'cozenLib.textarea',
+            'cozenLib.tooltip',
+            'cozenLib.view',
+            'cozenLib.icons',
+            'cozenLib.lazyLoad'
+        ]);
+
+})(window.angular);
